038da89d220096b8317ed5870822089b3d8999ad
Julia-Frost <juliafrostwork@gmail.com>
2022-08-10 16:00:45 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const inputEl = document.querySelector("#name-input");
+const nameEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", () => {
+  nameEl.textContent = inputEl.value;
+  console.log(nameEl);
+});

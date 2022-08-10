0dbbbee730a82467338763e6ab13dc7b31a1cb15
Julia-Frost <juliafrostwork@gmail.com>
2022-08-10 15:53:23 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -2,5 +2,15 @@ const decrementBtn = document.querySelector("button[data-action='decrement']");
 const incrementBtn = document.querySelector("button[data-action='increment']");
 const value = document.querySelector("#value");
 
-decrementBtn.addEventListener("click", () => (value.textContent = value));
-console.log(value);
+let counterValue = Number(value.textContent);
+counterValue = 0;
+
+decrementBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+});
+
+incrementBtn.addEventListener("click", () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+});

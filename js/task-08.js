40c78f4396f0481451c42c4a5f50c8029b7fbd96
Julia-Frost <juliafrostwork@gmail.com>
2022-08-12 20:11:31 +0300

    add 9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const buttonSubmit = document.querySelector("button");
+const inputEl = document.querySelectorAll("input");
 
+buttonSubmit.addEventListener("submit", (event) => event.preventDefault());
+inputEl.addEventListener("input", () => {
+  console.log(inputEl.value);
+  if (inputEl.value === "") {
+    alert;
+  }
+});
+
+console.log(inputEl);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+const textEl = document.querySelector(".color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+button.addEventListener("click", () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  textEl.textContent = `${getRandomHexColor()}`;
+});

f66674969c617d8737c01ff8559887d1b0fff24d
Julia-Frost <juliafrostwork@gmail.com>
2022-08-15 23:08:29 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,30 +2,28 @@ const formEl = document.querySelector(".login-form");
 const buttonSubmit = document.querySelector("button");
 const inputEl = document.querySelectorAll("input");
 
-formEl.addEventListener("submit", (event) => event.preventDefault());
+formEl.addEventListener("submit", onFormSubmit);
 
-const formElements = event.currentTarget.elements;
-console.log(formElements);
-const email = formElements.email.value;
-console.log(email);
-const password = formElements.password.value;
-console.log(password);
-// function checkLogin() {
-//   event.preventDefault();
-// }
-const newInputArray = [...inputArray];
-// console.log(newInputArray);
-// const checkedInput = newInputArray.every((inputEl, newInputArray) =>
-//   console.log(inputEl)
-// );
-// console.log(checkedInput);
-form.addEventListener("submit", callbackForm);
-function callbackForm(event) {
+function onFormSubmit(event) {
   event.preventDefault();
-}
-buttonSubmit.addEventListener("click", callbackCheck);
-function callbackCheck() {
-  if (checkedInput === true) {
-    console.log("gdsfgzdrsg");
+
+  // const formData = new FormData(event.currentTarget);
+
+  // formData.forEach((value, name) => {
+  //   console.log(name, value);
+  // });
+
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = {
+    email,
+    password,
+  };
+  if (email === "" || password === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    console.log(formData);
   }
 }

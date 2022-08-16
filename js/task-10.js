0732e92dfa4433df87d352a0f4e42b4a9dc6c678
Julia-Frost <juliafrostwork@gmail.com>
2022-08-16 23:02:41 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
+let createBtn = document.querySelector("[data-create]");
+console.log(createBtn);
+let destroyBtn = document.querySelector("[data-destroy]");
+console.log(destroyBtn);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

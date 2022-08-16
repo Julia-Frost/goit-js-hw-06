8ae61bc5589e7b2824188927244971e1f7f0ed45
Julia-Frost <juliafrostwork@gmail.com>
2022-08-16 23:12:21 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -5,6 +5,15 @@ console.log(createBtn);
 let destroyBtn = document.querySelector("[data-destroy]");
 console.log(destroyBtn);
 
+numberInput.addEventListener("click, ");
+createBtn.addEventListener("click", () => {
+  console.log("zacreatsilos");
+});
+
+destroyBtn.addEventListener("click", () => {
+  console.log("zadestroilos");
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
